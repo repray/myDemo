11ca0c0c9e371eeb54bb24812a78b413fe0cb654
Li.Yang <806446856@qq.com>
2021-08-10 19:21:45 +0800

    Initial commit

---
+++ src/main/java/com/liang/demo/TomcatConfig.java
@@ -0,0 +1,26 @@
+package com.liang.demo;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/8/10.
+ * @Desc tomcat配置,暂时不清楚做什么的
+ */
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((Connector connector) -> {
+            connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+            connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+        });
+        return factory;
+    }
+}
