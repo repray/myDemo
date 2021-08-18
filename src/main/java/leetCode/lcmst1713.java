3c5a7b5bf239ddf96d454b48dc29eb9d25e0b938
Li.Yang <806446856@qq.com>
2021-08-18 12:28:55 +0800

    feat: add 面试题17-13

---
+++ src/main/java/leetCode/lcmst1713.java
@@ -0,0 +1,13 @@
+package leetCode;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/8/18.
+ * @Desc 面试题 17.13. 恢复空格  https://leetcode-cn.com/problems/re-space-lcci/
+ */
+public class lcmst1713 {
+
+    public int respace(String[] dictionary, String sentence) {
+        return 1;
+    }
+}
