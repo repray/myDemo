1fac20850862aed2e97ad4b0b81b7334c28bf5a3
Li.Yang <806446856@qq.com>
2021-08-18 14:58:43 +0800

    feat: add lc1018

---
+++ src/main/java/leetCode/lc1018.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+import java.util.List;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/8/18.
+ * @Desc 1018. 可被 5 整除的二进制前缀 https://leetcode-cn.com/problems/binary-prefix-divisible-by-5/
+ */
+public class lc1018 {
+
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        return null;
+    }
+}
