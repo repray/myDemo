ac18602d7574b1a6bce8fc15134f072e740eab81
Li.Yang <806446856@qq.com>
2021-08-25 14:28:54 +0800

    feat: 添加lc14 注释

--- src/main/java/leetCode/lc14.java
+++ src/main/java/leetCode/lc14.java
@@ -16,14 +16,17 @@ public class lc14 {
     }
 
     public static String longestCommonPrefix(String[] strs) {
+        //先判断数组是否为空,为空则直接返回
         if (strs == null || strs.length == 0) {
             return "";
         }
+        //将数组第一位设置为前缀
         String res = strs[0];
         for (String str : strs) {
-            //表示必须从0开始包含
+            //用indexOf的返回结果判断前缀是否一致
             while (str.indexOf(res) != 0) {
-                //开始滑动
+                //如果不一致,则说明该索引不是公共索引,将索引长度调整-1
+                //如果res是最小长度的值,也不包含索引,则说明该数组没有公共前缀,不是最小长度则慢慢缩减res的值进行比较
                 res = res.substring(0, res.length() - 1);
             }
         }
