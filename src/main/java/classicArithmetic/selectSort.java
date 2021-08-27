f88ff95f7921ff70b32523e992a90aaca949ebce
Li.Yang <806446856@qq.com>
2021-08-27 19:19:02 +0800

    feat: 更新 选择排序

--- src/main/java/classicArithmetic/selectSort.java
+++ src/main/java/classicArithmetic/selectSort.java
@@ -1,29 +1,37 @@
 package classicArithmetic;
 
+import java.util.Arrays;
+
 /**
  * @Author Li Yang
  * @Created on 2021/8/27.
  * @Desc 选择排序
- * 待吸收理解
  */
 public class selectSort {
 
+    public static void main(String[] args) {
+        int array[] = {38, 21, 15, 48, 91, 3, 62, 79, 86, 54};
+        System.out.println(Arrays.toString(selectSort(array)));
+    }
 
-    public static int[] selectSort(int[] a) {
-        int n = a.length;
-        for (int i = 0; i < n - 1; i++) {
-            int min = i;
-            for (int j = i + 1; j < n; j++) {
-                if (a[min] > a[j]) {
-                    min = j;
+    public static int[] selectSort(int[] array) {
+        //按数组长度循环
+        for (int i = 0; i < array.length; i++) {
+            //先将第一位的值定义为比较值,也就是最小的值
+            int min = array[i];
+            //再将数组按长度循环,开始值为i+1,就是每次比较第一位与后面所有位的值
+            for (int j = i + 1; j < array.length; j++) {
+                //如果最小位大于后面某位的值,则将两个位置的值替换
+                if (min > array[j]) {
+                    int temp;
+                    temp = min;
+                    min = array[j];
+                    array[j] = temp;
                 }
             }
-            //交换
-            int temp = a[i];
-            a[i] = a[min];
-            a[min] = temp;
-        }
-        return a;
-
+            //完成一次内圈循环后,min的值则为本次循环最小的值,将a[i]更新为min值参与下次比较
+            array[i] = min;
+        }//完成所有外圈循环后,输出的值则是从小到大排序的有序数组
+        return array;
     }
 }
