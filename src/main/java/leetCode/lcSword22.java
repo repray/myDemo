b75386bfb547ed5fec2848819535a9be9ace7dfd
liyang <806446856@qq.com>
2021-09-02 21:43:45 +0800

    add lcSword22

---
+++ src/main/java/leetCode/lcSword22.java
@@ -0,0 +1,27 @@
+package leetCode;
+
+/**
+ * @author : liyang
+ * @date : 2021/9/2
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+
+public class lcSword22 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
