f776944a4455f586d8f1d7a87a4091a9da6a7bdc
Li.Yang <806446856@qq.com>
2021-09-03 11:11:10 +0800

    更新: lcSWord22 添加注释

--- src/main/java/leetCode/lcSword22.java
+++ src/main/java/leetCode/lcSword22.java
@@ -9,26 +9,34 @@ package leetCode;
 
 public class lcSword22 {
     public static void main(String[] args) {
+        //声明链表
         ListNode firstNode = new ListNode(1);
         ListNode secondNode = new ListNode(2);
         ListNode thitdNode = new ListNode(3);
         ListNode fourthNode = new ListNode(4);
+        //将链表用next方法链接
         firstNode.next = secondNode;
         secondNode.next = thitdNode;
         thitdNode.next = fourthNode;
+        //输出链表的值
         System.out.println(getKthFromEnd(firstNode, 3).val);
     }
 
     public static ListNode getKthFromEnd(ListNode head, int k) {
-        ListNode fast = head, slow = head;
+        //采用快慢双指针解法,声明一个快指针,一个慢指针
+        ListNode fast = head;
+        ListNode slow = head;
+        //快指针先行k步
         while (k > 0) {
             fast = fast.next;
             k--;
         }
+        //当k<=0时,快慢指针一起进行,当快指针走到链表劲头,慢指针的位置就是倒数k所在的位置
         while (fast != null) {
             fast = fast.next;
             slow = slow.next;
         }
+        //返回慢指针所在的位置
         return slow;
     }
 
