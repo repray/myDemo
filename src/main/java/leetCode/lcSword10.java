82ac0bb8d75747ee5c4553b1e7483d4bdb48bd47
liyang <806446856@qq.com>
2021-09-04 23:22:17 +0800

    新增 lcSword10

---
+++ src/main/java/leetCode/lcSword10.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+/**
+ * @author : liyang
+ * @date : 2021/9/4
+ * 剑指 Offer 10- I. 斐波那契数列
+ * https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+ */
+public class lcSword10 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int fib(int n) {
+        return 1;
+    }
+}
