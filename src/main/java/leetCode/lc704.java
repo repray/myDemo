06c56ef38087ebcfa24aef8f0c5b471d5311fb02
liyang <806446856@qq.com>
2021-09-06 23:41:50 +0800

    新增 lc704

---
+++ src/main/java/leetCode/lc704.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+/**
+ * @author : liyang
+ * @date : 2021/9/6
+ * link: https://leetcode-cn.com/problems/binary-search/
+ */
+public class lc704 {
+    public static void main(String[] args) {
+
+    }
+
+    public int search(int[] nums, int target) {
+        return 1;
+    }
+}
