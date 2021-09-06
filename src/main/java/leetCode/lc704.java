963b90d83dbd6947d648948d27bf9aa433192cc1
liyang <806446856@qq.com>
2021-09-07 00:04:49 +0800

    更新 lc704

--- src/main/java/leetCode/lc704.java
+++ src/main/java/leetCode/lc704.java
@@ -7,10 +7,25 @@ package leetCode;
  */
 public class lc704 {
     public static void main(String[] args) {
-
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = -2;
+        System.out.println(search(nums, target));
     }
 
-    public int search(int[] nums, int target) {
-        return 1;
+    public static int search(int[] nums, int target) {
+        int head = 0;
+        int tail = nums.length - 1;
+
+        while (head <= tail) {
+            int mid = head + (tail - head) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                head = mid + 1;
+            } else if (nums[mid] > target) {
+                tail = mid - 1;
+            }
+        }
+        return -1;
     }
 }
