c1951babf1638521b86c1d4a143ef7051f4f1721
liyang <806446856@qq.com>
2021-09-17 23:43:59 +0800

    更新 lc14test

--- src/test/java/leetCode/lc14Test.java
+++ src/test/java/leetCode/lc14Test.java
@@ -7,12 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * @author : liyang
  * @date : 2021/9/16
+ *  lc14测试类
  */
 class lc14Test {
 
     @Test
     void testLongestCommonPrefix() {
         String[] strs = {"flower","flow","flight"};
+        //assertEquals方法,根据传入的"expected"参数和待测试方法的返回值坐判断,返回测试结果
         assertEquals("fl", new lc14().longestCommonPrefix(strs));
     }
 }
