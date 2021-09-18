fe35c5cc0b85d39fafc320490f7c7d413a7e1b86
liyang <806446856@qq.com>
2021-09-18 21:49:41 +0800

    新增 lc292

---
+++ src/main/java/leetCode/lc292.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+/**
+ * @author : liyang
+ * @date : 2021/9/18
+ * link: https://leetcode-cn.com/problems/nim-game/
+ */
+public class lc292 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canWinNim(int n) {
+        return true;
+    }
+}
