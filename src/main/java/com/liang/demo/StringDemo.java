6307456187eafe110456b055008577cd614416cf
liyang <806446856@qq.com>
2021-09-19 23:41:05 +0800

    更新 StringDemo

--- src/main/java/com/liang/demo/StringDemo.java
+++ src/main/java/com/liang/demo/StringDemo.java
@@ -27,5 +27,16 @@ public class StringDemo {
         //这里要注意,进行replace操作以后,需要对word进行重新赋值
         word = word.replace("Hello","Hi");
         System.out.println(word);
+        //charAt方法,返回下标对应的字符
+        System.out.println(word.charAt(0));
+        //String的toCharArray方法可以将string类按位拆分为char类型的数组
+        char[] chars = word.toCharArray();
+        System.out.println(Arrays.toString(chars));
+        // indexOf 获取当前字符串中的指定字符的第一次出现的位置,lastIndexOf 获取当前字符串中的指定字符的最后一次出现的位置,算法中常用
+        System.out.println(word.indexOf("H")+","+word.lastIndexOf("H"));
+        //toLowerCase 将字符串全部转为小写
+        System.out.println(word.toLowerCase());
+        //toUpperCase 将字符串全部转为大写
+        System.out.println(word.toUpperCase());
     }
 }
