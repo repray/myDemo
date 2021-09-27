94176c2df2c0e20a1abfa415beb0a34386fd8e21
Li.Yang <806446856@qq.com>
2021-09-27 20:52:24 +0800

    新增 DealBlank类,处理字符串中不等长的空格为统一长度的空格

---
+++ src/main/java/com/liang/demo/DealBlank.java
@@ -0,0 +1,30 @@
+package com.liang.demo;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/9/27.
+ * @Desc 处理字符串中不等长的空格为统一长度的空格
+ */
+public class DealBlank {
+    public static void main(String[] args) {
+        
+    }
+
+    //处理文字中的空格,全部替换为6个下划线______
+    public static String dealBlank(String word) {
+        int ll = word.indexOf('_');
+        char[] chars = word.toCharArray();
+        String blankLength = "_";
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '_' && chars[i + 1] == '_') {
+                blankLength = blankLength + "_";
+            }
+            if (chars[i] == '_' && chars[i + 1] != '_') {
+                break;
+            }
+
+        }
+        word = word.replace(blankLength, "______");
+        return word;
+    }
+}
