bae9ef491c6c7462e208382d9c31140f3b2e06f5
Li.Yang <806446856@qq.com>
2021-09-30 21:25:24 +0800

    新增 mqttDemo

---
+++ src/main/java/com/liang/demo/MqttDemo/MqttReciveDemo.java
@@ -0,0 +1,13 @@
+package com.liang.demo.MqttDemo;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/9/30.
+ * @Desc
+ */
+public class MqttReciveDemo {
+    public static void main(String[] args) {
+        Server.recieve("world");
+
+    }
+}
