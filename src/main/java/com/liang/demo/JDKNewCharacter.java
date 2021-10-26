9ba84a878a02fc6ea840dc87e3c20fd12cb3f0da
Li.Yang <806446856@qq.com>
2021-10-26 16:00:35 +0800

    更新 JDKNewCharacter user 实体类改名

--- src/main/java/com/liang/demo/JDKNewCharacter.java
+++ src/main/java/com/liang/demo/JDKNewCharacter.java
@@ -20,21 +20,21 @@ public class JDKNewCharacter {
         List<User> list = new ArrayList<>();
         //定义三个用户对象
         User user1 = new User();
-        user1.setName("liyang");
+        user1.setName("praye");
         user1.setPassword(123456);
         User user2 = new User();
-        user2.setName("zhuang");
+        user2.setName("liness");
         user2.setPassword(234567);
         User user3 = new User();
-        user3.setName("haixiao");
+        user3.setName("txvo");
         user3.setPassword(456789);
         //添加用户到集合中
         list.add(user1);
         list.add(user2);
         list.add(user3);
 
-        //在集合中查询用户名为huxiansen的集合
-        List<User> userList = list.stream().filter(user -> "liyang".equals(user.getName())).collect(Collectors.toList());
+        //在集合中查询用户名为praye的集合
+        List<User> userList = list.stream().filter(user -> "praye".equals(user.getName())).collect(Collectors.toList());
         //在集合中查询出第一个用户密码为123456的用户
         Optional<User> user = list.stream().filter(userTemp -> userTemp.getPassword().equals(123456)).findFirst();
 
