4099776ba413cd999d108f020f5b77e86a7d5d77
liyang <806446856@qq.com>
2021-11-01 23:53:33 +0800

    新增 lc575

---
+++ src/main/java/leetCode/lc575.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+/**
+ * @author : liyang
+ * @date : 2021/11/1
+ * https://leetcode-cn.com/problems/distribute-candies/
+ */
+public class lc575 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int distributeCandies(int[] candyType) {
+        return 1;
+    }
+}
