e436af8c325b761101bdec15e45632fb22b61ce0
Li.Yang <806446856@qq.com>
2021-11-09 20:12:53 +0800

    新增 LeetCode21

---
+++ src/main/java/com/liang/demo/ListNode.java
@@ -0,0 +1,25 @@
+package com.liang.demo;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/11/9.
+ * @Desc
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
---
+++ src/main/java/leetcode/LeetCode21.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import com.liang.demo.ListNode;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @Author Li Yang
+ * @Created on 2021/11/9.
+ * @Desc https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ */
+public class LeetCode21 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        Set candyTypeSort = new TreeSet();
+        return null;
+    }
+}
