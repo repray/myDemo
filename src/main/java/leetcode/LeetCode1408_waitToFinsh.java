e676bea3039058dad60efcafbf4bedd151947ce7
liyang <806446856@qq.com>
2021-11-11 23:48:04 +0800

    新增 LeetCode1408 待完成

---
+++ src/main/java/leetcode/LeetCode1408_waitToFinsh.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * @author : liyang
+ * @date : 2021/11/11
+ * link: https://leetcode-cn.com/problems/string-matching-in-an-array/
+ */
+public class LeetCode1408_waitToFinsh {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> stringMatching(String[] words) {
+        return null;
+    }
+}
