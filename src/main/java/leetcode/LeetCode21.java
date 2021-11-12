2a46fe51748688d517b4f0bdb7fd26e04722709e
Li.Yang <806446856@qq.com>
2021-11-12 20:24:51 +0800

    更新 LeetCode21

--- src/main/java/leetcode/LeetCode21.java
+++ src/main/java/leetcode/LeetCode21.java
@@ -1,14 +1,11 @@
 package leetcode;
 
-import com.liang.demo.ListNode;
-
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * @Author Li Yang
  * @Created on 2021/11/9.
- * @Desc https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * @Desc 合并两个有序链表
+ * link https://leetcode-cn.com/problems/merge-two-sorted-lists/
  */
 public class LeetCode21 {
 
@@ -16,8 +13,57 @@ public class LeetCode21 {
 
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        Set candyTypeSort = new TreeSet();
-        return null;
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode sentinel = new ListNode(0);
+        ListNode result = sentinel;
+        merge(l1, l2, sentinel);
+        return result.next;
+
     }
+
+    public static void merge(ListNode l1, ListNode l2, ListNode result) {
+        if (l1 == null) {
+            result.next = l2;
+            return;
+        }
+        if (l2 == null) {
+            result.next = l1;
+            return;
+        }
+        if (l1.val < l2.val) {
+            result.next = new ListNode(l1.val);
+            l1 = l1.next;
+        } else {
+            result.next = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        result = result.next;
+        merge(l1, l2, result);
+
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
 }
