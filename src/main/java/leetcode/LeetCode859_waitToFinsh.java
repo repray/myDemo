b85dc7d40ff697ee97842151e8366b1ca339f4e6
liyang <806446856@qq.com>
2021-11-23 23:28:01 +0800

    新增 LeetCode859 待完成

---
+++ src/main/java/leetcode/LeetCode859_waitToFinsh.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * @author : liyang
+ * @date : 2021/11/23
+ * link: https://leetcode-cn.com/problems/buddy-strings/
+ */
+public class LeetCode859_waitToFinsh {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean buddyStrings(String s, String goal) {
+        return true;
+    }
+}
