fdc050a6c61f74140a863e256d8187267f18db40
Li.Yang <806446856@qq.com>
2021-12-17 22:34:14 +0800

    更新 LeetCode1518

---
+++ src/main/java/leetcode/Leetode1518.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * @Author Li Yang
+ * @Created on 17/12/2021.
+ * @Desc 1518. 换酒问题
+ * link: https://leetcode-cn.com/problems/water-bottles/
+ * 小区便利店正在促销，用 numExchange 个空酒瓶可以兑换一瓶新酒。你购入了 numBottles 瓶酒。
+ * 如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
+ * 请你计算 最多 能喝到多少瓶酒。
+ */
+public class Leetode1518 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        return 1;
+    }
+}
