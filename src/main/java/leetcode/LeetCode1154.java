979ced3fa9fe28369cc31fe5250f87827bda6f97
liyang <806446856@qq.com>
2021-12-21 23:52:43 +0800

    新增 LeetCode1154

---
+++ src/main/java/leetcode/LeetCode1154.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * @author : liyang
+ * @date : 21/12/2021
+ * link: https://leetcode-cn.com/problems/day-of-the-year/
+ * 1154. 一年中的第几天
+ */
+public class LeetCode1154 {
+    public static void main(String[] args) {
+
+    }
+
+    public int dayOfYear(String date) {
+        return 1;
+    }
+}
