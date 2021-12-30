71d120ea57ef42041d5788c25723ac65e5275fe2
Li.Yang <806446856@qq.com>
2021-12-30 17:49:09 +0800

    更新 LeetCode1518

--- src/main/java/leetcode/Leetode1518.java
+++ src/main/java/leetcode/Leetode1518.java
@@ -12,10 +12,20 @@ package leetcode;
 public class Leetode1518 {
 
     public static void main(String[] args) {
-
+        System.out.println(numWaterBottles(15,4));
     }
 
-    public int numWaterBottles(int numBottles, int numExchange) {
-        return 1;
+    public static int numWaterBottles(int numBottles, int numExchange) {
+        //第一次买的酒设置为能喝到酒的初始值
+        int result = numBottles;
+        //如果酒瓶的数量>=能换酒瓶的数量,就开始进行换酒的操作
+        while (numBottles >= numExchange) {
+            //换酒的时候,将初始值添加本次可以换的酒的数量,也就是酒瓶数/多少瓶就可以兑换一瓶酒的数量
+            result += numBottles / numExchange;
+            //此时剩下的空瓶数量为可以换的酒瓶数量,加上没有达到换一整瓶酒的剩下的瓶子
+            numBottles = numBottles / numExchange +numBottles % numExchange;
+            //然后进行下一次判断
+        }
+        return result;
     }
 }
