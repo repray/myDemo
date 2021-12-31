da0e40dbb40171b2a08071e89c5c4c395deff2e1
Li.Yang <806446856@qq.com>
2021-12-31 15:44:29 +0800

    add LeetCode507

---
+++ src/main/java/leetcode/LeetCode507.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * @Author Li Yang
+ * @Created on 31/12/2021.
+ * @Desc 507. 完美数
+ * link: https://leetcode-cn.com/problems/perfect-number/
+ * 对于一个 正整数，如果它和除了它自身以外的所有 正因子 之和相等，我们称它为 「完美数」。
+ * 给定一个 整数 n， 如果是完美数，返回 true，否则返回 false
+ * <p>
+ * 输入：num = 28
+ * 输出：true
+ * 解释：28 = 1 + 2 + 4 + 7 + 14
+ * 1, 2, 4, 7, 和 14 是 28 的所有正因子。
+ */
+public class LeetCode507 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean checkPerfectNumber(int num) {
+        return true;
+    }
+
+
+}
