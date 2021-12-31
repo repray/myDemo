15c4a735a021cdbf5a1b6b67105b4d334c19e67a
Li.Yang <806446856@qq.com>
2021-12-31 17:02:55 +0800

    更新 LeetCode1816 推荐解法

--- src/main/java/leetcode/LeetCode507.java
+++ src/main/java/leetcode/LeetCode507.java
@@ -16,11 +16,21 @@ package leetcode;
 public class LeetCode507 {
 
     public static void main(String[] args) {
-
+        System.out.println(checkPerfectNumber(8128));
     }
 
-    public boolean checkPerfectNumber(int num) {
-        return true;
+    public static boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum+ i;
+            }
+        }
+        if (sum == num) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
