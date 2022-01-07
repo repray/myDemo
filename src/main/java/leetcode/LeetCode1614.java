0e90d206c0c1d8d758d1105130fe537918fa3203
Li.Yang <806446856@qq.com>
2022-01-07 15:49:38 +0800

    add LeetCode1614

---
+++ src/main/java/leetcode/LeetCode1614.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * @Author Li Yang
+ * @Created on 7/1/2022.
+ * @Desc
+ */
+public class LeetCode1614 {
+    public static void main(String[] args) {
+        String s = "123";
+        System.out.println(maxDepth(s));
+    }
+
+    public static int maxDepth(String s) {
+        return 0;
+    }
+}
