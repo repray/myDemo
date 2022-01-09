599fe5e1d051be8e69dd0bc18461be1ba040e9e6
liyang <806446856@qq.com>
2022-01-10 00:02:31 +0800

    新增 LeetCode1629

---
+++ src/main/java/leetcode/LeetCode1629_unfinish.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * @author : liyang
+ * @date : 10/1/2022
+ * link: https://leetcode-cn.com/problems/slowest-key/
+ */
+public class LeetCode1629_unfinish {
+    public static void main(String[] args) {
+
+    }
+
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        return 1;
+    }
+}
