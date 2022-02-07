a599f2131bc36d76828d75aeb4891875f130d032
Li.Yang <806446856@qq.com>
2022-02-07 19:09:39 +0800

    更新 LeetCode13

--- src/main/java/leetcode/LeetCode13.java
+++ src/main/java/leetcode/LeetCode13.java
@@ -1,5 +1,10 @@
 package leetcode;
 
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author Li Yang
  * @Created on 15/12/2021.
@@ -9,10 +14,35 @@ package leetcode;
 public class LeetCode13 {
 
     public static void main(String[] args) {
-
+        System.out.println(romanToInt("MDCXCV"));
     }
 
-    public int romanToInt(String s) {
-        return 1;
+    public static int romanToInt(String s) {
+        HashMap<String, Integer> romaNum = new HashMap<>();
+        romaNum.put("I", 1);
+        romaNum.put("V", 5);
+        romaNum.put("X", 10);
+        romaNum.put("L", 50);
+        romaNum.put("C", 100);
+        romaNum.put("D", 500);
+        romaNum.put("M", 1000);
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+           int num = romaNum.get(String.valueOf(s.charAt(i)));
+            int nextNum = 0;
+            if (i != s.length() - 1) {
+                nextNum = romaNum.get(String.valueOf(s.charAt(i + 1)));
+            }
+            if (num > nextNum) {
+                result += num;
+            } else if (num < nextNum) {
+                result = result + (nextNum - num);
+                i++;
+            } else {
+                result += num;
+            }
+        }
+        return result;
     }
 }
