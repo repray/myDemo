ec0ecd240a8a580aec904418274fc81433f39363
Li.Yang <806446856@qq.com>
2022-02-20 23:58:12 +0800

    更新 LeetCode13

---
+++ src/main/java/leetcode/LeetCode717.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * @Author Li Yang
+ * @Created on 20/2/2022.
+ * link: https://leetcode-cn.com/problems/1-bit-and-2-bit-characters/
+ * @Desc 717. 1比特与2比特字符
+ */
+public class LeetCode717 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isOneBitCharacter(int[] bits) {
+        return true;
+    }
+}
