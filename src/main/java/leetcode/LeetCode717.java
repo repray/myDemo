81f9387e068d39b83beb39efe6161bd6ca6dbde5
Li Yang <liyangjk@crhms.cn>
2024-07-29 18:17:25 +0800

    update LeetCode717 unfinish

--- src/main/java/leetcode/LeetCode717.java
+++ src/main/java/leetcode/LeetCode717.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @Author Li Yang
  * @Created on 20/2/2022.
@@ -8,10 +12,33 @@ package leetcode;
  */
 public class LeetCode717 {
     public static void main(String[] args) {
-
+        int[] bits = {1,0};
+        System.out.println(isOneBitCharacter(bits));
     }
 
     public static boolean isOneBitCharacter(int[] bits) {
-        return true;
+        List<Integer> list = Arrays.stream(bits).boxed().collect(Collectors.toList());
+        if (list.size()==1&&list.get(0)==0) return true;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == 1 && list.get(i + 1) == 1) {
+                list.remove(i);
+                list.remove(i);
+                i += 1;
+                continue;
+            }
+            if (list.get(i) == 1 && list.get(i + 1) == 0) {
+                list.remove(i);
+                list.remove(i);
+                i += 1;
+                continue;
+            }
+            if (list.get(i) == 0) {
+                list.remove(i);
+            }
+        }
+        if (list.get(0) != 0) {
+            return false;
+        } else
+            return true;
     }
 }
