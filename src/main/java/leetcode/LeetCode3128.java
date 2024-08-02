ce063e4e40f3d6d2c11d07aef327d6f9090cce29
Li Yang <liyangjk@crhms.cn>
2024-08-02 11:17:03 +0800

    add LeetCode3128

---
+++ src/main/java/leetcode/LeetCode3128.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * @Author: Li Yang
+ * @CreateTime: 2024-08-02
+ * @Description: 3128 直角三角形
+ * link: https://leetcode.cn/problems/right-triangles/description/
+ */
+public class LeetCode3128 {
+    public static void main(String[] args) {
+        int[][] grid = {{0, 1, 0}, {0, 1, 1}, {0, 1, 0}};
+    }
+
+    public static long numberOfRightTriangles(int[][] grid) {
+        return 0l;
+    }
+}
