e8c9c205bb3cabfe9d25b79fcb1545be76f63531
liang <li.praye@outlook.com>
2025-02-14 20:30:26 +0800

    更像 leetCode21

--- src/main/java/leetcode/LeetCode21.java
+++ src/main/java/leetcode/LeetCode21.java
@@ -13,39 +13,55 @@ public class LeetCode21 {
 
     }
 
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        ListNode sentinel = new ListNode(0);
-        ListNode result = sentinel;
-        merge(l1, l2, sentinel);
-        return result.next;
+//    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        if (l1 == null) {
+//            return l2;
+//        }
+//        if (l2 == null) {
+//            return l1;
+//        }
+//        ListNode sentinel = new ListNode(0);
+//        ListNode result = sentinel;
+//        merge(l1, l2, sentinel);
+//        return result.next;
+//    }
+//
+//    public static void merge(ListNode l1, ListNode l2, ListNode result) {
+//        if (l1 == null) {
+//            result.next = l2;
+//            return;
+//        }
+//        if (l2 == null) {
+//            result.next = l1;
+//            return;
+//        }
+//        if (l1.val < l2.val) {
+//            result.next = new ListNode(l1.val);
+//            l1 = l1.next;
+//        } else {
+//            result.next = new ListNode(l2.val);
+//            l2 = l2.next;
+//        }
+//        result = result.next;
+//        merge(l1, l2, result);
+//    }
 
-    }
-
-    public static void merge(ListNode l1, ListNode l2, ListNode result) {
-        if (l1 == null) {
-            result.next = l2;
-            return;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
         }
-        if (l2 == null) {
-            result.next = l1;
-            return;
+        if (list2 == null) {
+            return list1;
         }
-        if (l1.val < l2.val) {
-            result.next = new ListNode(l1.val);
-            l1 = l1.next;
+        ListNode temp = new ListNode();
+        if (list1.val < list2.val) {
+            temp = list1;
+            temp.next = mergeTwoLists(list1.next, list2);
         } else {
-            result.next = new ListNode(l2.val);
-            l2 = l2.next;
+            temp = list2;
+            temp.next = mergeTwoLists(list1, list2.next);
         }
-        result = result.next;
-        merge(l1, l2, result);
-
+        return temp;
     }
 
     public static class ListNode {
