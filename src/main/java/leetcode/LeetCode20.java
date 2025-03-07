d3869be447bbba80ade5bc86a490af64926eacae
Li Yang <liyang314@gome.com.cn>
2025-03-07 13:57:13 +0800

    更像 leetCode20

--- src/main/java/leetcode/LeetCode20.java
+++ src/main/java/leetcode/LeetCode20.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * @Author Li Yang
  * @Created on 2021/8/30.
@@ -9,29 +11,68 @@ package leetcode;
 public class LeetCode20 {
 
     public static void main(String[] args) {
-        String s = "(){}}{";
+//        String s = "([}}])";
+        String s = "()[]{}";
         System.out.println(isValid(s));
     }
 
     public static boolean isValid(String s) {
-
         if (s.length() <= 1 || s.length() >= 10000) {
             return false;
         }
-        if (s.length() == 2 && (String.valueOf(s.charAt(0)).equals(")") || String.valueOf(s.charAt(0)).equals("}") || String.valueOf(s.charAt(0)).equals("]"))) {
+        if ("{}".equals(s) || "()".equals(s) || "[]".equals(s)) {
+            return true;
+        } else if (s.length() == 2 && (")".equals(String.valueOf(s.charAt(0))) || "}".equals(String.valueOf(s.charAt(0))) || "]".equals(String.valueOf(s.charAt(0))))) {
+            return false;
+        } else if (s.length() % 2 != 0) {
             return false;
         }
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (String.valueOf(s.charAt(i)).equals("(") && (!String.valueOf(s.charAt(i + 1)).equals(")") && s.lastIndexOf(")") != s.length() - 1 - i)) {
-                return false;
+        Stack<String> stack = new Stack<>();
+        try {
+            for (int i = 0; i < s.length(); i++) {
+                if ("(".equals(String.valueOf(s.charAt(i)))) {
+                    stack.push("(");
+                    continue;
+                }
+                if (")".equals(String.valueOf(s.charAt(i))) && "(".equals(stack.peek())) {
+                    stack.pop();
+                    continue;
+                }
+                if ("{".equals(String.valueOf(s.charAt(i)))) {
+                    stack.push("{");
+                    continue;
+                }
+                if ("}".equals(String.valueOf(s.charAt(i))) && "{".equals(stack.peek())) {
+                    stack.pop();
+                    continue;
+                }
+                if ("[".equals(String.valueOf(s.charAt(i)))) {
+                    stack.push("[");
+                    continue;
+                }
+                if ("]".equals(String.valueOf(s.charAt(i))) && "[".equals(stack.peek())) {
+                    stack.pop();
+                    continue;
+                }
+                if (")".equals(String.valueOf(s.charAt(i)))) {
+                    if (stack.search("{") == -1) {
+                        return false;
+                    }
+                }
+                if ("]".equals(String.valueOf(s.charAt(i)))) {
+                    if (stack.search("[") == -1) {
+                        return false;
+                    }
+                }
+                if ("}".equals(String.valueOf(s.charAt(i)))) {
+                    if (stack.search("{") == -1) {
+                        return false;
+                    }
+                }
             }
-            if (String.valueOf(s.charAt(i)).equals("{") && (!String.valueOf(s.charAt(i + 1)).equals("}") && s.lastIndexOf("}") != s.length() - 1 - i)) {
-                return false;
-            }
-            if (String.valueOf(s.charAt(i)).equals("[") && (!String.valueOf(s.charAt(i + 1)).equals("]") && s.lastIndexOf("]") != s.length() - 1 - i)) {
-                return false;
-            }
+            return stack.empty();
+        } catch (Exception exception) {
+            return false;
         }
-        return true;
     }
 }
